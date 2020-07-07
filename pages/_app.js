6f5208ac97f4311f750be3de05e46ba28470d5ca
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2020-07-07 03:44:38 -0500

    tailwind working

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 
+import '../styles/index.css';
+
 const mdComponents = {
   h1: props => <h1 style={{ color: 'tomato' }} {...props} />,
 };
